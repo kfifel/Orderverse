cd9aa81f09df01e7f19fca76966f6fdadf9a527a
khalid fifel <fifle.khalid99@gmail.com>
2024-03-14 09:48:29 +0000

    config the client eureka microservices

--- Order Management/src/main/java/com/youcode/order_management/OrderManagementApplication.java
+++ Order Management/src/main/java/com/youcode/order_management/OrderManagementApplication.java
@@ -2,8 +2,10 @@ package com.youcode.order_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderManagementApplication {
 
     public static void main(String[] args) {
--- Product Management/src/main/java/com/youcode/productmanagement/ProductManagementApplication.java
+++ Product Management/src/main/java/com/youcode/productmanagement/ProductManagementApplication.java
@@ -2,8 +2,10 @@ package com.youcode.productmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductManagementApplication {
 
     public static void main(String[] args) {
